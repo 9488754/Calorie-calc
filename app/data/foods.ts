5f494efa4f6a05a57dd28d7d63c4607dc87f82cb
Wiktor Gudzowski <u63_wikgud_lbn@technischools.com>
2026-05-02 10:23:23 +0200

    feat: added 6 new fruits

--- app/data/foods.ts
+++ app/data/foods.ts
@@ -30,4 +30,59 @@ export type food = {
       alt: "zdjecie borowek",
       caloriesPer1g: 0.57,
     },
+
+    {
+        name: "Czarna porzeczka",
+        image: "/images/blackberry.png",
+        alt: "zdjecie porzeczek",
+        caloriesPer1g: 0.43,
+      },
+
+
+      {
+        name: "Winogrona",
+        image: "/images/grape.png",
+        alt: "zdjecie winogron",
+        caloriesPer1g: 0.66,
+      },
+
+
+      {
+        name: "Borowki",
+        image: "/images/blueberry.png",
+        alt: "zdjecie borowek",
+        caloriesPer1g: 0.57,
+      },
+
+
+      {
+        name: "Kiwi",
+        image: "/images/kiwi.png",
+        alt: "zdjecie kiwi",
+        caloriesPer1g: 0.61,
+      },
+
+
+      {
+        name: "Mango",
+        image: "/images/mango.png",
+        alt: "zdjecie mango",
+        caloriesPer1g: 0.60,
+      },
+
+
+      {
+        name: "Maliny",
+        image: "/images/raspberry.png",
+        alt: "zdjecie malin",
+        caloriesPer1g: 0.53,
+      },
+
+
+      {
+        name: "Ananas",
+        image: "/images/pineapple.png",
+        alt: "zdjecie ananasa",
+        caloriesPer1g: 0.50,
+      },
   ];
